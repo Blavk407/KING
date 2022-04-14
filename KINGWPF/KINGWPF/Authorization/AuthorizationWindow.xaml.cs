9ded9e7031df35bcd23c38cb1add3f1f2a642320
Blavk407 <82106986+Blavk407@users.noreply.github.com>
2022-04-14 13:43:18 +0500

    первые шаги малыша

---
+++ KINGWPF/KINGWPF/Authorization/AuthorizationWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using KINGWPF.Classes;
+
+namespace KINGWPF.Authorization
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+            AllContent.Visibility = Visibility.Hidden;     
+        }
+
+        private void WindowBorder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void AuthorizationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private async void RegistrationTextblock_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            await Loader.LoadAsync(Dispatcher, AllContent, LoadingMediaElement, async () => registrationWindow.ShowDialog());
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //await Loader.LoadAsync(Dispatcher, AllContent, LoadingMediaElement, async () => { });
+        }
+    }
+}
---
+++ KINGWPF/KINGWPF/Classes/Loader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace KINGWPF.Classes
+{
+    internal class Loader
+    {
+        public static async Task LoadAsync(Dispatcher dispatcher,Grid AllContentGrid, MediaElement LoadingMediaElement,Func<Task> asyncFunc)
+        {
+            dispatcher.Invoke(() => AllContentGrid.Visibility = Visibility.Hidden);
+            dispatcher.Invoke(() => { LoadingMediaElement.Position = TimeSpan.FromTicks(1); LoadingMediaElement.Play(); LoadingMediaElement.Visibility = Visibility.Visible; });
+            await Task.Delay(5000);
+            await asyncFunc();
+            dispatcher.Invoke(() => AllContentGrid.Visibility = Visibility.Visible);
+            dispatcher.Invoke(() => { LoadingMediaElement.Stop(); LoadingMediaElement.Visibility = Visibility.Hidden; });
+        }
+    }
+}
